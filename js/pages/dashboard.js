e86fa02aade6cbe6d1725b6ae3fe71f64a8fe0e6
thiagoarruda09 <thiago.arruda.09@hotmail.com>
2025-03-06 19:57:16 -0300

    adiciona um contador de modulos

--- js/pages/dashboard.js
+++ js/pages/dashboard.js
@@ -1,5 +1,41 @@
 function dashboard (){
     return`
+    
+    <section class="card card-body">
+        <div class="alert alert-info">
+            <strong>Dica:</strong>
+            Selecione apenas os modulos que você quer nesse sistema
+        </div>
+        <div class="row">
+            <div class="col"><div class="form-check form-switch">
+                <input onchange="MudarDeModulo (this)" class="form-check-input" type="checkbox" role="switch" id="modulo_categoria">
+                <label class="form-check-label" for="modulo_categoria">Categorias</label>
+              </div>
+              <div class="form-check form-switch">
+                <input onchange="MudarDeModulo (this)" class="form-check-input" type="checkbox" role="switch" id="modulo_clientes">
+                <label class="form-check-label" for="modulo_clientes">Clientes</label>
+              </div>
+              <div class="form-check form-switch">
+                <input onchange="MudarDeModulo (this)" class="form-check-input" type="checkbox" role="switch" id="modulo_paises">
+                <label class="form-check-label" for="modulo_paises">paises</label>
+              </div>
+              <div class="form-check form-switch">
+                <input onchange="MudarDeModulo (this)" class="form-check-input" type="checkbox" role="switch" id="modulo_produtos">
+                <label class="form-check-label" for="modulo_produtos">produtos</label>
+              </div>
+              <div class="form-check form-switch">
+                <input onchange="MudarDeModulo (this)" class="form-check-input" type="checkbox" role="switch" id="modulo_pedidos">
+                <label class="form-check-label" for="modulo_pedidos">pedidos</label>
+              </div></div>
+            <div class="col">
+                <div class="alert alert-secondary">
+                    <span class="fs-1" id="contador_de_modulo"> 0 </span>modulos
+                </div>
+            </div>
+        </div>
+        
+
+    </section>
       <div class="d-flex">
     
         
